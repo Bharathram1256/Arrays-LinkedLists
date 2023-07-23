c8bf93556467866ecd1622169ada33d94f529944
Bharathram1256 <maruboyinibharathkumar@gmail.com>
2023-07-23 12:24:46 +0530

    patch

--- negetive.java
+++ negetive.java
@@ -1,26 +1,26 @@
-import java.io.*;
 import java.util.*;
 
 public class negetive {
 
     public static void main(String[] args) {
     
-    Scanner sc = new Scanner(System.in);
-    int a = sc.nextInt();
-    List<Integer> list = new ArrayList<>();
-    for(int i = 0; i < a; i++) {
-            list.add(sc.nextInt());
-        }
-    int negetive = 0;
-    for(int i = 0; i < list.size(); i++){
-        int sum = 0;
-        for(int j = i; j < list.size(); j++){
-            sum += list.get(j);
-            if(sum < 0){
-                negetive ++;
+    try (Scanner sc = new Scanner(System.in)) {
+        int a = sc.nextInt();
+        List<Integer> list = new ArrayList<>();
+        for(int i = 0; i < a; i++) {
+                list.add(sc.nextInt());
+            }
+        int negetive = 0;
+        for(int i = 0; i < list.size(); i++){
+            int sum = 0;
+            for(int j = i; j < list.size(); j++){
+                sum += list.get(j);
+                if(sum < 0){
+                    negetive ++;
+                }
             }
         }
+        System.out.println(negetive);
     }
-    System.out.println(negetive);
 }
 }
