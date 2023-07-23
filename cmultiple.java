7c71bbff0419607c3e4ffb15563798f11634d028
Bharathram1256 <maruboyinibharathkumar@gmail.com>
2023-07-23 22:39:42 +0530

    3

--- cmultiple.java
+++ cmultiple.java
@@ -8,38 +8,39 @@ public class cmultiple{
 
 
         
-        Scanner sc = new Scanner(System.in); 
-        int n = sc.nextInt();     //code to take the size of the array from the user.
-        int arr[] = new int[n]; 
-        //for taking the input from the user.
-        //the code below only takes the integers as input as the array elements.
-        try {
-            for(int i =0;i<arr.length;i++){
-            arr[i]=sc.nextInt();
-            }           
-        } catch (Exception e) {
-            // TODO: handle exception
-            System.err.println("Please give the input as integers only.");
-        }
-        
+        try (Scanner sc = new Scanner(System.in)) {
+            int n = sc.nextInt();     //code to take the size of the array from the user.
+            int arr[] = new int[n]; 
+            //for taking the input from the user.
+            //the code below only takes the integers as input as the array elements.
+            try {
+                for(int i =0;i<arr.length;i++){
+                arr[i]=sc.nextInt();
+                }           
+            } catch (Exception e) {
+                // TODO: handle exception
+                System.err.println("Please give the input as integers only.");
+            }
+            
 
-        //code for printing the array normal array.
-        // for(int i = 0;i<arr.length;i++){
-        //     System.out.println(arr[i]);
-        // }
-        // sc.close();
+            //code for printing the array normal array.
+            // for(int i = 0;i<arr.length;i++){
+            //     System.out.println(arr[i]);
+            // }
+            // sc.close();
 
-        //code to calculate cumulative multiple at each index.
-        for(int j = 1;j<arr.length;j++){
-            arr[j] = arr[j-1]*arr[j];
-        }    
+            //code to calculate cumulative multiple at each index.
+            for(int j = 1;j<arr.length;j++){
+                arr[j] = arr[j-1]*arr[j];
+            }    
 
 
-        //code to print the updated array
+            //code to print the updated array
 
-        System.out.println(" ");
-        for(int k = 0; k<arr.length;k++){
-            System.out.println(arr[k]);
+            System.out.println(" ");
+            for(int k = 0; k<arr.length;k++){
+                System.out.println(arr[k]);
+            }
         }
     }
 }
