6a1f29bf4df045bbefbe618c83663001de85afde
Bharathram1256 <maruboyinibharathkumar@gmail.com>
2023-07-23 12:09:55 +0530

    4.negetive

---
+++ negetive.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class negetive {
+
+    public static void main(String[] args) {
+    
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    List<Integer> list = new ArrayList<>();
+    while(a > 0){
+        a--;
+        list.add(sc.nextInt());
+    }
+    int negetive = 0;
+    for(int i = 0; i < list.size(); i++){
+        int sum = 0;
+        for(int j = i; j < list.size(); j++){
+            sum += list.get(j);
+            if(sum < 0){
+                negetive ++;
+            }
+        }
+    }
+    System.out.println(negetive);
+}
+}
